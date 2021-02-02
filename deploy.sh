8f0c13732a92bb9953b37798d376334a182885e6
gnakatani <77743273+gnakatani@users.noreply.github.com>
2021-02-02 12:27:41 -0800

    removed SHAs and put kubectl commands on one line deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,12 @@
 # Build all images, tag
-docker build -t gnakatani/multi-client:latest -t gnakatani/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t gnakatani/multi-server:latest -t gnakatani/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t gnakatani/multi-worker:latest -t gnakatani/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t gnakatani/multi-client:latest -f ./client/Dockerfile ./client
+docker build -t gnakatani/multi-server:latest -f ./server/Dockerfile ./server
+docker build -t gnakatani/multi-worker:latest -f ./worker/Dockerfile ./worker
 # push latest to docker hub
 docker push gnakatani/multi-client:latest
 docker push gnakatani/multi-server:latest
 docker push gnakatani/multi-worker:latest
-# push specific version to docker hub
-docker push gnakatani/multi-client:$SHA
-docker push gnakatani/multi-server:$SHA
-docker push gnakatani/multi-worker:$SHA
 # apply all configs in k8s
 kubectl apply -f k8s
 # 
-kubectl set image deployments/server-deployment server=gnakatani/multi-server:$SHA kubectl set image deployments/client-deployment client=gnakatani/multi-client:$SHA kubectl set image deployments/worker-deployment worker=gnakatani/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=gnakatani/multi-server kubectl set image deployments/client-deployment client=gnakatani/multi-client kubectl set image deployments/worker-deployment worker=gnakatani/multi-worker
