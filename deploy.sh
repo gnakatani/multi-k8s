ea93c711d2d9fff9dc821ddb9253280d58c01182
gnakatani <77743273+gnakatani@users.noreply.github.com>
2021-02-02 12:06:27 -0800

    put ALL commands on same line in deploy.sh and removed comments

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,1 @@
-docker build -t gnakatani/multi-client -t gnakatani/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t gnakatani/multi-worker -f ./worker/Dockerfile ./worker
-docker push gnakatani/multi-client:latest
-docker push gnakatani/multi-client:$SHA
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-server:$SHA
-docker push gnakatani/multi-worker
-kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server kubectl set image deployments/client-deployment client=gnakatani/multi-client kubectl set image deployments/worker-deployment worker=gnakatani/multi-worker
+docker build -t gnakatani/multi-client:latest -t gnakatani/multi-client:$SHA -f ./client/Dockerfile ./client docker build -t gnakatani/multi-server:latest -t gnakatani/multi-server:$SHA -f ./server/Dockerfile ./server docker build -t gnakatani/multi-worker:latest -t gnakatani/multi-worker:$SHA -f ./worker/Dockerfile ./worker docker push gnakatani/multi-client:latest docker push gnakatani/multi-server:latest docker push gnakatani/multi-worker:latest docker push gnakatani/multi-client:$SHA docker push gnakatani/multi-server:$SHA docker push gnakatani/multi-worker:$SHA kubectl apply -f k8s kubectl set image deployments/server-deployment server=gnakatani/multi-server:$SHA kubectl set image deployments/client-deployment client=gnakatani/multi-client:$SHA kubectl set image deployments/worker-deployment worker=gnakatani/multi-worker:$SHA
