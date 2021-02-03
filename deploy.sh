4fa5c229497448cc0f3ce62d9469e1bea7ed2654
gnakatani <77743273+gnakatani@users.noreply.github.com>
2021-02-03 08:52:53 -0800

    using stephengrider deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,16 @@
-# Build all images, tag
-docker build -t gnakatani/multi-client:latest -t gnakatani/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t gnakatani/multi-server:latest -t gnakatani/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t gnakatani/multi-worker:latest -t gnakatani/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-# push latest to docker hub
-docker push gnakatani/multi-client:latest
-docker push gnakatani/multi-server:latest
-docker push gnakatani/multi-worker:latest
-# push specific version to docker hub
-docker push gnakatani/multi-client:$SHA
-docker push gnakatani/multi-server:$SHA
-docker push gnakatani/multi-worker:$SHA
-# apply all configs in k8s
-kubectl apply -f k8s 
-kubectl set image deployments/server-deployment server=gnakatani/multi-server 
-kubectl set image deployments/client-deployment client=gnakatani/multi-client 
-kubectl set image deployments/worker-deployment worker=gnakatani/multi-worker
+docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push stephengrider/multi-client:latest
+docker push stephengrider/multi-server:latest
+docker push stephengrider/multi-worker:latest
+
+docker push stephengrider/multi-client:$SHA
+docker push stephengrider/multi-server:$SHA
+docker push stephengrider/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
+kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
